6185aa10fd1695777607a895702e7439107f0d13
Slienfore <Slienfore@163.com>
2024-05-05 22:26:09 +0800

    feat: styled-components 的 (css in js) 写法添加样式

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,23 @@
-import styles from "./ListGroup.module.css";// 引入 css
 import { useState } from "react";
+// import styles from "./ListGroup.module.css"; // 引入 css
+import styled from "styled-components"; // 引入 styled-component
+
+// styled.?`` -> 返回生成 React组件
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  // 根据 props 的值进行 控制
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
+
 interface Props {
   items: string[];
   heading: string;
@@ -9,7 +27,7 @@ interface Props {
 function ListGroup({ items, heading, onSelectItem }: Props) {
   // useState(initialState)
   // set 函数，例如 setSomething(nextState)
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
@@ -17,24 +35,20 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
       {/* 避免 null 渲染 */}
       {!items.length && <p>No item found</p>}
 
-      <ul className={[styles.listGroup, styles.container].join(' ')}>
+      <List>
         {items.map((item, index) => (
-          <li
+          <ListItem
+            active={selectedIndex === index}
             key={item}
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
             onClick={() => {
               setSelectedIndex(index);
               onSelectItem(item); // 子向父传值
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
