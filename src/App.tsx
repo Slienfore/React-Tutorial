aec0934c3e7b22f7060a7102e0c60c9a4041e2e9
Slienfore <Slienfore@163.com>
2024-05-05 21:17:30 +0800

    feat: 控制 Alert 组件的显示与隐藏

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log("Click!")} color="primary">
+      {/* && 进行控制 组件的显示与隐藏 */}
+      {alertVisible && (
+        // 传递 onClose 回调进行关闭
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Alert exercise...
+        </Alert>
+      )}
+
+      {/* 按钮控制 alert 显示与隐藏 */}
+      <Button onClick={() => setAlertVisibility(true)} color="primary">
         My Button
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,25 @@
 import { ReactNode } from "react";
 
 interface Props {
-  children: ReactNode;// children 使用 ReactNode进行接收
+  children: ReactNode; // children 使用 ReactNode进行接收
+  onClose: () => void; // 关闭按钮回调
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  // 添加 关闭按钮 和 .alert-dismissible 类,
+  // 这将在警告框 (alert) 组件的右侧增加额外的空间并放置关闭按钮。
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        onClick={onClose}
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
