1957ca288a5b728f2b8c06ff562398c3c6613db8
Slienfore <Slienfore@163.com>
2024-05-05 20:12:24 +0800

    feat: props -> 父向子传值

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 import ListGroup from "./components/ListGroupList"; // 引入组件
 
 function App() {
+  const items = ["China", "New York", "Tokyo", "London", "San Francisco"];
+
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading="Cities"></ListGroup>
     </div>
   );
 }
--- src/components/ListGroupList.tsx
+++ src/components/ListGroupList.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  const items = ["China", "New York", "Tokyo", "London", "San Francisco"];
-
+function ListGroup({ items, heading }: Props) {
   // useState(initialState)
   // set 函数，例如 setSomething(nextState)
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List Compo</h1>
+      <h1>{heading}</h1>
       {/* 避免 null 渲染 */}
       {!items.length && <p>No item found</p>}
 
