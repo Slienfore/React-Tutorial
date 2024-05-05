481f9477b8561c9fba96bc52f9baef5550055aae
Slienfore <Slienfore@163.com>
2024-05-05 23:59:32 +0800

    feat: React Icons usage

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,12 @@ import "./App.css";
 import ListGroup from "./components/ListGroup/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
-  const items = ["China", "New York", "Tokyo", "London", "San Francisco"];
-
-  // 子向父传值, 传递函数
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div style={{backgroundColor: 'skyblue'}}>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+    <div>
+      <BsFillCalendarFill color='#9d5da9' size='40'/>
     </div>
   );
 }
