848d11ec11bd7dbcf66e4d86bfa272af37384717
Slienfore <Slienfore@163.com>
2024-05-06 11:29:47 +0800

    feat: React use Immer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,35 @@ import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
 import { BsFillCalendarFill } from "react-icons/bs";
 import Like from "./components/Like";
+import { produce } from "immer";
 
 function App() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Big" },
+    { id: 2, title: "Small" },
+  ]);
+
+  const handleClick = () => {
+    // setBugs( bugs.map((item) => (item.id === 2 ? { ...item, title: "Bug" } : item)) );
+
+    setBugs(
+
+      produce((draft) => {
+
+        const bug = draft.find((item) => item.id === 2)!;
+        bug.title = "Bug";
+
+      })
+      
+    );
+  };
+
   return (
-    <div>
-      <Like onClick={() => console.log("clicked")}></Like>
-    </div>
+    <>
+      {bugs}
+      <Like onClick={handleClick}></Like>
+      <Like onClick={handleClick}></Like>
+    </>
   );
 }
 
