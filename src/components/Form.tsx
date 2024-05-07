16806619832d3ed4589b0c557a91a45d58d70f7d
Slienfore <Slienfore@163.com>
2024-05-07 17:47:49 +0800

    feat: define button state of disabled

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,31 +14,46 @@ const schema = z.object({
 type Person = z.infer<typeof schema>; // auto infer TS type
 
 const Form = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<Person>({ resolver: zodResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Person>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
-        <input {...register("name")}
-            id="name" type="text" className="form-control"/>
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
 
         {/* {errors.name?.type === "required" && (<p className="text-danger">The name is required!</p>)} */}
         {errors.name && <p className="text-danger">{errors.name.message}</p>}
-
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input {...register("age", { valueAsNumber: true })}
-          id="age" type="number" className="form-control"/>
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
 
         {/* {errors.age?.type === "required" && (<p className="text-danger">The age is required!</p>)} */}
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
-        
       </div>
-      <button className="btn btn-primary" type="submit">
+      {/* define button state of disabled */}
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
