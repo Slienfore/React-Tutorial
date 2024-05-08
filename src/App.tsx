e9cdc8c136522ffb1e528752413f41eb4d184fbc
Slienfore <Slienfore@163.com>
2024-05-08 15:19:36 +0800

    feat: useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,21 @@
-import { useState } from "react";
-import "./App.css";
-import ListGroup from "./components/ListGroup/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import { BsFillCalendarFill } from "react-icons/bs";
-import Like from "./components/Like";
-import { produce } from "immer";
-import { useImmer } from "use-immer";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
-import ExpenseList from "./components/ExpanseTracker/components/ExpenseList";
-import ExpenseFilter from "./components/ExpanseTracker/components/ExpenseFilter";
-import ExpenseForm from "./components/ExpanseTracker/components/ExpenseForm";
+import { useRef, useEffect } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useImmer([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Entertainment" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ]);
+  // afterRender
+  useEffect(() => {
+    // Side effect
+    ref.current?.focus();
+  });
 
-  // if user select the category, it will filter target items, or will return origin expenses
-  // why not use the state？ -> once user select the category, will trigger the 'setSelectedCategory' changing, view will render again
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((item) => item.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    document.title = "React Hook of useEffect";
+  });
 
   return (
     <>
-      <div className="mb-4">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses((draft) => {
-              // ExpenseForm 传递过来的 expense 没有 id, 需要设置
-              draft.push({ id: draft.length + 1, ...expense });
-            })
-          }
-        ></ExpenseForm>
-      </div>
-
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category: string) => setSelectedCategory(category)}
-        ></ExpenseFilter>
-      </div>
-
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id: number) =>
-          setExpenses((draft) => {
-            const objIndex = draft.findIndex((item) => item.id === id);
-            draft.splice(objIndex, 1); // delete item from list
-          })
-        }
-      ></ExpenseList>
+      <input ref={ref} type="text" className="form-control" />
     </>
   );
 }
